43ec082a37685a022eaf56072a61966134e151e3
Alaap008 <alaapbanerjee08@gmail.com>
2019-06-12 20:16:39 +0530

    Some changes in KYC Model

--- models/kycModels.js
+++ models/kycModels.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 
 const kycSchema =new mongoose.Schema({
+user:{
+        id: {type: mongoose.Schema.Types.ObjectId,
+            required: true, ref:'Users'},
+        name: {type: String,
+            required: true, ref:'Users'},
+},
 adhaarno:{type: Number, required:true},
 adhaarImage: {type: String, required:true},
 panno:{type: String, required:true},
