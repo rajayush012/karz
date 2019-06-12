24c607953dc90cb8231cfca2910077ada2e4416e
Alaap008 <alaapbanerjee08@gmail.com>
2019-06-12 15:45:27 +0530

    User Model Added

---
+++ models/userModels.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema =new mongoose.Schema({
+    name : {type: String, required: true},
+    username : {type: String, required: true},
+    password : {type: String, required: true},
+    loans: {type: Number, default: 0},
+    pendingLoans:{type: Number, default: 0},
+    wallet: {type: Number, default: 0},
+    
+})
+
+module.exports = mongoose.model('Users', userSchema);
