531e05ff194945e64fb260f670c414e9d1056b2d
Alaap008 <alaapbanerjee08@gmail.com>
2019-06-13 23:26:13 +0530

    Some Changes

--- routes/admin.js
+++ routes/admin.js
@@ -2,53 +2,31 @@ const express = require('express');
 const router  = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/userModels');
-const Admin= require('../models/adminModel');
 const passport = require('passport');
+const Admin = require()
 
 router.get('/new',(req,res)=>{
     res.render('admin/newadmin')
 });
 
-router.post('/new',(req,res)=>{
-    var newAdmin = new Admin({username: req.body.username, name: req.body.name});
-    Admin.register(newAdmin,req.body.password, (err,user)=>{
+router.post('/new',(req,res)=>{   
+    var newUser = new User({username: req.body.username, name: req.body.name, isAdmin: 'Yes'});
+    User.register(newUser,req.body.password, (err,user)=>{
         if(err){
             console.log(err);
             res.redirect('/')
         }
         passport.authenticate("local")(req,res, ()=>{
-        res.redirect(`admin/dashboard`)
+        res.render(`admin/continue`);
         })
     } );
 
 })
-
-router.get('/dashboard',isLoggedIn,(req,res)=>{
-    Admin.findById(req.user._id,(err,admin)=>{
-        if(err){
-            console.log(err);
-        }else{
-            res.render('admin/dashboard',{admin:admin});
-        }
-    })
+router.post('/newAdmin',(req,res)=>{
+    Admin.create
 })
 
-router.get('/login',(req,res)=>{
-    res.render('admin/login')
-});
 
-
-router.post('/login',passport.authenticate("local",{
-    successRedirect: "admin/dashboard",
-    failureRedirect: "admin/login"
-}),(req,res)=>{
-    
-});
-
-router.get('/logout',(req,res)=>{
-    req.logOut();
-    res.redirect('/');
-})
 
 function isLoggedIn(req,res,next){
     // console.log(req.isAuthenticated());
