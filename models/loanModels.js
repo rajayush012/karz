37e447ba1431fa953cd3ddd4ad136f7ab00867b3
Ayush Raj <raj.ayush012@gmail.com>
2019-06-13 16:21:34 +0530

    added timer to installments

--- models/loanModels.js
+++ models/loanModels.js
@@ -1,24 +1,19 @@
 const mongoose = require('mongoose');
 
 const loanSchema =new mongoose.Schema({
-    recepient:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
+    recepient:{type:mongoose.Schema.Types.ObjectId,ref: 'User'},
     collablender: [{
             _id: {type: mongoose.Schema.Types.ObjectId,
                  ref: 'User'},
             amtcontrib: {type: Number}
                  
         }],
-    amtReq:{
-        type: Number, required: true
-    },
-    amtSatisfied:{
-        type: Number, default: 0
-    },
+    amtReq:{type: Number, required: true},
+    amtSatisfied:{type: Number, default: 0},
+    timeForBid:{type: Number, default: 15},
     dateRequested:{type: Date},
-    dateDue: {type: Date},
+    dateDue: {type: Number},
+    dateRemaining: {type:Number},
     status:{type: String, default: 'pending'}
 })
 
--- routes/loan.js
+++ routes/loan.js
@@ -1,86 +1,200 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/userModels');
 const passport = require('passport');
 const Loan = require('../models/loanModels')
 
 
-router.get('/showall',isLoggedIn,(req,res)=>{
-    Loan.find({},(err,loans)=>{
-        if(err){
+router.get('/showall', isLoggedIn, (req, res) => {
+    Loan.find({}, (err, loans) => {
+        if (err) {
             console.log(err);
-        }else{
-            var filterLoans = loans.filter(loan=>{
-                
+        } else {
+            var filterLoans = loans.filter(loan => {
+
                 return (!loan.recepient.equals(req.user._id));
             })
-            res.render('loan/all',{loans:filterLoans});
+            res.render('loan/all', { loans: filterLoans });
         }
     });
 });
 
-router.get('/new',isLoggedIn,(req,res)=>{
+router.get('/new', isLoggedIn, (req, res) => {
     res.render('loan/newloan');
 });
 
-router.post("/new",isLoggedIn,(req,res)=>{
+router.post("/new", isLoggedIn, (req, res) => {
     Loan.create({
         recepient: req.user._id,
         amtReq: req.body.amount,
         dateRequested: Date.now(),
-        dateDue: new Date(req.body.date),
-    }, (err,loan)=>{
-        if(err){
+        dateDue: req.body.date*30,
+        dateRemaining: req.body.date,
+    }, (err, loan) => {
+        if (err) {
             console.log(err);
             res.redirect('/loan/new');
-        }else{
-            User.findById(req.user._id,(err,user)=>{
-                if(err){
+        } else {
+            User.findById(req.user._id, (err, user) => {
+                if (err) {
                     console.log(err);
-                }else{
+                } else {
                     user.loanreq.push(loan._id);
                     user.save();
                     res.redirect(`/loan/${loan._id}`);
                 }
-                
+
             })
 
-
-           
         }
-        
+
     })
 })
 
-router.get('/:loanid',isLoggedIn,(req,res)=>{
-    Loan.findById(req.params.loanid,(err,loan)=>{
-        res.render('loan/loandetails',{loan:loan});
+router.get('/:loanid', isLoggedIn, (req, res) => {
+    Loan.findById(req.params.loanid, (err, loan) => {
+        res.render('loan/loandetails', { loan: loan });
     })
 })
 
-router.get('/:loanid/bid',isLoggedIn,(req,res)=>{
-    Loan.findById(req.params.loanid,(err,loan)=>{
-        if(err){
+router.get('/:loanid/bid', isLoggedIn, (req, res) => {
+    Loan.findById(req.params.loanid, (err, loan) => {
+        if (err) {
             console.log(err);
-        }else{
-            res.render('loan/bid',{loan});
+        } else {
+            res.render('loan/bid', { loan });
         }
     });
-   
+
 });
 
-router.post('/:loanid/bid',(req,res)=>{
-    res.render('loan/bidsuccess');
+router.post('/:loanid/bid', (req, res) => {
+    Loan.findById(req.params.loanid, (err, loan) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (req.body.amount <= (loan.amtReq - loan.amtSatisfied) && req.body.amount !== 0) {
+
+                User.findById(req.user._id, (err, user) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+
+                        if (user.wallet >= req.body.amount) {
+                            loan.collablender.push({ _id: user._id, amtcontrib: req.body.amount })
+                            let newsat = parseInt(loan.amtSatisfied) + parseInt(req.body.amount);
+                            loan.amtSatisfied = newsat;
+                            if (loan.amtSatisfied == loan.amtReq) {
+                                //console.log(loan.status);
+                                loan.status = 'accepted';
+                                User.findById(loan.recepient, (err, user) => {
+                                    if (err) {
+                                        console.log(user);
+                                    } else {
+                                        user.wallet += loan.amtReq;
+                                    }
+
+                                });
+                            }
+                            user.wallet = parseInt(user.wallet) - parseInt(req.body.amount);
+                            loan.save();
+                            user.save();
+                            res.render('loan/bidsuccess');
+                        }
+                        else {
+                            res.redirect('/loan/showall');
+                        }
+
+
+                    }
+                })
+
+
+
+            }
+            else {
+                res.redirect('/loan/', req.params.loanid);
+            }
+
+
+        }
+    })
+
 })
 
-function isLoggedIn(req,res,next){
+function isLoggedIn(req, res, next) {
     // console.log(req.isAuthenticated());
-     if(req.isAuthenticated()){
-         return next();
-     }
-     res.redirect('/user/login');
- }
- 
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/user/login');
+}
+
+var dayDuration = 20000;
+
+var interTimer = setInterval(() => {
+    Loan.find({ status: 'pending' }, (err, loans) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (loans.length !== 0) {
+                loans.forEach(loan => {
+                    if (loan.timeForBid <= 0) {
+                        loan.status = 'declined';
+                    }
+                    (async () => {
+                        loan.timeForBid -= 1;
+                    })();
+
+                    loan.save();
+                })
+            } else {
+                clearInterval(interTimer);
+            }
+
+        }
+
+    });
+
+   
+}, dayDuration);
+
+var installTimer = setInterval(() => {
+    Loan.find({ status: 'accepted' }, (err, loans) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (loans.length !== 0) {
+                loans.forEach(loan => {
+                    if(loan.dateRemaining<0){
+                        loan.status='paid';
+                    }
+                    (async () => {
+                        loan.dateRemaining -= 1;
+                        User.findById(loan.recepient._id,(err,user)=>{
+                            if(err){
+                                console.log(err);
+                            }else{
+                               //logic for wallet deduction
+
+                               //logic for distribution of interest money to contributors
+                               
+                            }
+                        })
+                    })();
+
+                    loan.save();
+                })
+            } else {
+                clearInterval(installTimer);
+            }
+
+        }
+
+    });
+
+   
+}, dayDuration*30);
 
 module.exports = router;
