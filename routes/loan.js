5c1f757d4923d6e8b8fa361296c719881a871af5
Ayush Raj <raj.ayush012@gmail.com>
2019-06-15 22:23:30 +0530

    removed trash

--- routes/loan.js
+++ routes/loan.js
@@ -219,34 +219,7 @@ var interTimer = setInterval(() => {
 }, dayDuration);
 
 
-                        }
-                        else
-                        {
-                            loan.status = 'default';
-                            var transporter = nodemailer.createTransport({
-                                service: 'gmail',
-                                auth: {
-                                  user: 'alaapbanerjee08@gmail.com',
-                                  pass: 'ALAAP008'
-                                }
-                              });
-                              
-                              var mailOptions = {
-                                from: 'alaapbanerjee08@gmail.com',
-                                to: 'user.email',
-                                subject: 'Sending Email using Node.js',
-                                text: 'That was easy!'
-                              };
-                              
-                              transporter.sendMail(mailOptions, function(error, info){
-                                if (error) {
-                                  console.log(error);
-                                } else {
-                                  console.log('Email sent: ' + info.response);
-                                }
-                              });
-                        console.log("Hello Defaulter");
-                        }
+                  
 
 var installMentTimer = setInterval(()=>{
 
